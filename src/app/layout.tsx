079c6c916a9cd1b42454a5f4aedd0794c91c532c
harshpandey002 <coding.harshp@gmail.com>
2024-07-14 03:54:32 +0530

    Geist Font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { GeistSans } from 'geist/font/sans';
 import './globals.css';
+import { cn } from '@/lib/utils';
 
-const inter = Inter({ subsets: ['latin'] });
+const fontSans = GeistSans;
 
 export const metadata: Metadata = {
   title: 'Shadcn Theme Generator',
@@ -16,7 +17,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={cn(
+          'min-h-screen bg-background font-sans antialiased',
+          fontSans.variable
+        )}>
+        {children}
+      </body>
     </html>
   );
 }
