0efe3c6464af28432dd1f4149d48df1c4fb4e4ce
Harsh Pandey <coding.harshp@gmail.com>
2026-09-09 03:28:30 +0530

    security: remove hidden stealer payload from config file(s)

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,4 @@
 /** @type {import('next').NextConfig} */
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
 const nextConfig = {};
 
 export default nextConfig;
